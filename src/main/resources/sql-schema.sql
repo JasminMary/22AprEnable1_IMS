87dfc677014abf5eaae8b775d6f9623f84858cc6
Jasmin <jwillis2@qa.com>
2022-05-03 18:26:57 +0100

    Fulfilled user story for "As a user, I want to be able to create an item, so that I can add it can be added to orders."

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,4 +1,4 @@
---drop schema ims;
+drop schema ims;
 
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
@@ -9,4 +9,11 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+   `item_id` INT (11) NOT NULL AUTO_INCREMENT,
+   `item_name` VARCHAR (40) DEFAULT NULL,
+   `item_price` DOUBLE (8, 2) DEFAULT NULL,
+   PRIMARY KEY (`item_id`)
 );
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,15 @@
 DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `items`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+CREATE TABLE IF NOT EXISTS `items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `item_price` DOUBLE(8, 2) DEFAULT NULL,
+    PRIMARY KEY (`item_id`) 
 );
